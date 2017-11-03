99245c6510f01aa603fa07728cddf5400acbb22e
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-11-03 00:24:32 -0500

    Split classes into their own files and folder
    Classes are now their own file, as well as in the folder `/includes`.
    
    Change-Id: I6d0b3cbb1fee95b78b9df0bb05baf2e7670dc1d5

---
+++ includes/SkinDeskMessMirrored.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ *
+ * @ingroup Skins
+ */
+class SkinDeskMessMirrored extends SkinTemplate {
+	public $skinname = 'deskmessmirrored', $stylename = 'deskmessmirrored',
+		$template = 'DeskMessMirroredTemplate', $useHeadElement = true;
+
+	/**
+	 * Initializes Desk Mess Mirrored
+	 * @param OutputPage $out
+	 */
+	public function initPage( OutputPage $out ) {
+		parent::initPage( $out );
+
+		global $wgHooks;
+		// Add some required classes to the <body> element
+		$wgHooks['OutputPageBodyAttributes'][] = function( $out, $sk, &$bodyAttrs ) {
+			$bodyAttrs['class'] .= ' home blog custom-background';
+			return true;
+		};
+	}
+
+	function setupSkinUserCss( OutputPage $out ) {
+		global $wgStylePath;
+
+		parent::setupSkinUserCss( $out );
+
+		// Add CSS via ResourceLoader
+		$out->addModuleStyles( array(
+			'mediawiki.skinning.interface',
+			'mediawiki.skinning.content.externallinks',
+			'skins.deskmessmirrored'
+		) );
+
+		// Internet Explorer fixes (required for IE11 at least)
+		$out->addStyle( "{$wgStylePath}/DeskMessMirrored/css/ie.css", 'screen', 'IE' );
+	}
+}
