e2330dbeacf0b15367023b49677bc9c9be664650
zoranzoki21 <zorandori4444@gmail.com>
2020-08-27 16:00:22 +0200

    build: Replace jsonlint with eslint, add stylelint
    Change-Id: I5f316e13137306ad5046c2b080eca99dac28d4b5

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,26 @@
-/*jshint node:true */
+/* eslint-env node */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	grunt.loadNpmTasks( 'grunt-banana-checker' );
-
 	var conf = grunt.file.readJSON( 'skin.json' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 	grunt.initConfig( {
-		banana: conf.MessagesDirs,
-		jsonlint: {
+		eslint: {
+			options: {
+				extensions: [ '.js', '.json' ],
+				cache: true
+			},
+			all: '.'
+		},
+		stylelint: {
 			all: [
-				'**/*.json',
+				'**/*.css',
 				'!node_modules/**',
 				'!vendor/**'
 			]
-		}
+		},
+		banana: conf.MessagesDirs
 	} );
-
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
