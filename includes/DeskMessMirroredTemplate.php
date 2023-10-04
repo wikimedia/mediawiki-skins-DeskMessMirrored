98cde9db5df7acfeba75211b32547d352e7bc9a2
Jack Phoenix <ashley@uncyclomedia.co>
2023-10-04 19:48:25 +0300

    Add AfterPortlet support to support the new version of the NewsBox extension in MW 1.39+
    Change-Id: Ia16c07c2a5bdda97c6df4930f53de0f7e18113fb

--- includes/DeskMessMirroredTemplate.php
+++ includes/DeskMessMirroredTemplate.php
@@ -353,6 +353,16 @@ class DeskMessMirroredTemplate extends BaseTemplate {
 			echo $cont;
 		}
 		echo '</li>';
+
+		// Need this nonsense to support NewsBox in MW 1.39+ using the new hooks (urgh)
+		$content = $this->getSkin()->getAfterPortlet( $bar );
+		if ( $content !== '' ) {
+			echo Html::rawElement(
+				'div',
+				[ 'class' => [ 'after-portlet', 'after-portlet-' . $bar ] ],
+				$content
+			);
+		}
 	} // customBox()
 
 }
